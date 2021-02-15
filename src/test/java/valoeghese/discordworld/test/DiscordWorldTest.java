4e6e8f289b61f44d213718a35a09596ad03f634e
valoeghese <valoeghesemods@gmail.com>
2021-02-15 19:10:51 +1300

    test world gen

--- src/test/java/valoeghese/discordworld/test/DiscordWorldTest.java
+++ src/test/java/valoeghese/discordworld/test/DiscordWorldTest.java
@@ -1,12 +1,23 @@
 package valoeghese.discordworld.test;
 
-import net.dv8tion.jda.api.hooks.EventListener;
-import valoeghese.discordworld.Bootstrap;
+import java.io.File;
 
-public class DiscordWorldTest implements EventListener {
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import valoeghese.discordworld.Bootstrap;
+import valoeghese.discordworld.World;
+
+public class DiscordWorldTest extends ListenerAdapter {
 	public static void main(String[] args) {
 		Bootstrap.start(properties -> new DiscordWorldTest());
 	}
-	
-	
+
+	private World world;
+
+	@Override
+	public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
+		if (event.getMessage().getContentRaw().equals("start")) {
+			this.world = new World(event.getGuild(), 2, 2, new File("./guild.dat.zfg"));
+		}
+	}
 }
